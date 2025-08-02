e1170b88a2a45ac973757b2226fe8b77d8bf538d
reekystive <reekystive@gmail.com>
2025-08-02 20:31:06 +0800

    🔧 chore: replace uuid with nanoid

--- apps/app-quotum-server/src/db/schema.ts
+++ apps/app-quotum-server/src/db/schema.ts
@@ -1,11 +1,13 @@
-import { randomUUID } from 'crypto';
 import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { customAlphabet } from 'nanoid';
+
+const nanoid = customAlphabet('1234567890abcdef', 10);
 
 export const quotesTable = sqliteTable('quotes', {
   id: text('id')
     .primaryKey()
-    .$defaultFn(() => randomUUID()),
+    .$defaultFn(() => nanoid(10)),
   content: text('content').notNull(),
   title: text('title').notNull(),
   url: text('url').notNull(),
