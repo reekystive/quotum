a6091b1b5c1539ec0b3e30713daceafc83418220
reekystive <reekystive@gmail.com>
2025-08-05 00:16:42 +0800

    ✨ feat: remove vite-tsconfig-paths, and integrate @quotum/tailwindcss in app-quotum-extension

--- apps/app-quotum-extension/vite.config.ts
+++ apps/app-quotum-extension/vite.config.ts
@@ -1,4 +1,5 @@
 import { crx } from '@crxjs/vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import zip from 'vite-plugin-zip-pack';
@@ -8,6 +9,7 @@ import packageJson from './package.json' with { type: 'json' };
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     crx({ manifest }),
     zip({ outDir: 'release', outFileName: `crx-quotum-extension-${packageJson.version}.zip` }),
   ],
