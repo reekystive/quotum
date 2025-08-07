2a81a5dacde4970b49bfb2953bbd6cb4d4f07536
reekystive <reekystive@gmail.com>
2025-08-07 17:13:55 +0800

    🗃️ db: update database migrations

---
+++ apps/app-quotum-server/drizzle/0001_sloppy_namor.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `quotes` ADD `anchor_text_start` text;--> statement-breakpoint
+ALTER TABLE `quotes` ADD `anchor_text_end` text;--> statement-breakpoint
+ALTER TABLE `quotes` ADD `anchor_prefix` text;--> statement-breakpoint
+ALTER TABLE `quotes` ADD `anchor_suffix` text;
